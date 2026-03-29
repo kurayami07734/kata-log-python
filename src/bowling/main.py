72a83281dc8e81d2712525395b57b02b8856b627
Aditya Ghidora <adityaghidora@gmail.com>
2026-03-30 01:13:35 +0530

    feat: impl game class based on test scenario

--- src/bowling/main.py
+++ src/bowling/main.py
@@ -1,11 +1,32 @@
+from src.bowling.frame import Frame, FrameConcludedException
+
+
+class GameConcludedException(Exception):
+    """
+    Indicates that game has concluded
+    """
+
+
 class Game:
     def __init__(self) -> None:
-        self.frames = [[] for _ in range(10)]
+        self.frames: list[Frame] = [Frame(is_last_frame=(i == 9)) for i in range(10)]
+        self.current_frame = 0
 
     @property
     def frame_count(self):
         return len(self.frames)
 
-    def roll(self, pin_count: int) -> None: ...
+    def roll(self, pin_count: int) -> None:
+        if self.current_frame > 9:
+            raise GameConcludedException()
 
-    def score(self) -> int: ...
+        frame = self.frames[self.current_frame]
+
+        try:
+            frame.roll(pin_count)
+        except FrameConcludedException:
+            self.current_frame += 1
+            self.roll(pin_count)
+
+    def score(self) -> int:
+        return sum(f.score() for f in self.frames)
--- tests/bowling/test_bowling_game.py
+++ tests/bowling/test_bowling_game.py
@@ -1,7 +1,74 @@
-from src.bowling.main import Game
+import pytest
+
+from src.bowling.main import Game, GameConcludedException
 
 
 def test_game_has_10_frames():
     game = Game()
 
     assert game.frame_count == 10
+
+
+def test_game_scenario():
+    game = Game()
+
+    # Frame 1
+    game.roll(10)
+    game.roll(10)
+    game.roll(2)
+
+    assert game.score() == 22
+
+    # Frame 2
+    game.roll(8)
+    game.roll(2)
+
+    assert game.score() == 32
+
+    # Frame 3
+    game.roll(7)
+    game.roll(3)
+
+    assert game.score() == 42
+
+    # Frame 4
+    game.roll(5)
+    game.roll(5)
+
+    assert game.score() == 52
+
+    # Frame 5
+    game.roll(7)
+    game.roll(3)
+
+    assert game.score() == 62
+
+    # Frame 6
+    game.roll(7)
+    game.roll(3)
+
+    assert game.score() == 72
+    # Frame 7
+    game.roll(7)
+    game.roll(3)
+
+    assert game.score() == 82
+    # Frame 8
+    game.roll(7)
+    game.roll(3)
+
+    assert game.score() == 92
+    # Frame 9
+    game.roll(7)
+    game.roll(3)
+
+    assert game.score() == 102
+
+    # Frame 10
+    game.roll(7)
+    game.roll(3)
+
+    assert game.score() == 112
+
+    with pytest.raises(GameConcludedException):
+        game.roll(7)
