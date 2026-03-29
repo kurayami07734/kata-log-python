78bcf7a9c6e0261b07fa83173d6b47e89c038ed5
Aditya Ghidora <adityaghidora@gmail.com>
2026-03-30 00:56:43 +0530

    feat: impl frame class based on tests

--- src/bowling/frame.py
+++ src/bowling/frame.py
@@ -1,3 +1,34 @@
+class FrameConcludedException(Exception):
+    """
+    Indicates that a frame has exhausted its rolls
+    """
+
+
 class Frame:
     def __init__(self, is_last_frame=False) -> None:
-        pass
+        self.is_last_frame = is_last_frame
+        self.roll_count = 2
+        self.rolls: list[int] = []
+
+    def __grant_spares(self):
+        has_all_tens = all(r == 10 for r in self.rolls)
+        should_limit_rolls = len(self.rolls) > 2 and self.is_last_frame
+
+        if has_all_tens and not should_limit_rolls:
+            self.roll_count += 1
+
+    def roll(self, pin_count: int) -> None:
+        if pin_count < 0 or pin_count > 10:
+            raise ValueError(f"Illegal pin_count passed ({pin_count})")
+
+        if not self.roll_count:
+            raise FrameConcludedException()
+
+        self.rolls.append(pin_count)
+        self.roll_count -= 1
+
+        if not self.roll_count:
+            self.__grant_spares()
+
+    def score(self) -> int:
+        return sum(self.rolls)
--- tests/bowling/test_frame.py
+++ tests/bowling/test_frame.py
@@ -1,12 +1,24 @@
 import pytest
 
-from src.bowling.frame import Frame
+from src.bowling.frame import Frame, FrameConcludedException
 
 
 def test_frame_has_2_rolls():
     frame = Frame()
 
     assert frame.roll_count == 2
+
+
+def test_frame_validates_pin_count():
+    frame = Frame()
+
+    with pytest.raises(ValueError):
+        frame.roll(-1)
+
+    with pytest.raises(ValueError):
+        frame.roll(11)
+
+    assert frame.roll(2) is None
 
 
 def test_frame_stops_rolls():
@@ -37,3 +49,12 @@ def test_last_frame_allows_max_3_rolls():
 
     with pytest.raises(FrameConcludedException):
         frame.roll(1)
+
+
+def test_frame_score_is_accurate():
+    frame = Frame()
+
+    frame.roll(10)
+    frame.roll(8)
+
+    assert frame.score() == 18
